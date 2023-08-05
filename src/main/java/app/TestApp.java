b4d4e61ba8cccb82eef1c39fee748e7f81d5a669
James <jamessport6@gmail.com>
2023-08-05 14:02:13 +0800

    第12章

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -25,11 +25,19 @@ public class TestApp {
 //		app.insert(member);
 //		System.out.println(member.getId());
 		
-		System.out.println(app.deleteById(5));
+//		System.out.println(app.deleteById(5));
 		
+		Member member = new Member();
+//		member.setId(1);
+//		member.setPass(false);
+//		member.setRoleId(2);
+		
+//		System.out.println(app.updateById(member));
+		
+		System.out.println(app.selectById(2).getNickname());
 	}
 	
-	protected Integer insert(Member member) {
+	public Integer insert(Member member) {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
 		try {
@@ -44,7 +52,7 @@ public class TestApp {
 		}
 	}
 	
-	protected int deleteById(Integer id) {
+	public int deleteById(Integer id) {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
 		try {
@@ -58,6 +66,49 @@ public class TestApp {
 			e.printStackTrace();
 			session.getTransaction().rollback();
 			return -1;
+		}
+	}
+	
+	public int updateById(Member newmember) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member oldmember = session.get(Member.class, newmember.getId());
+			
+			final Boolean pass = newmember.getPass();
+			if(pass != null) {
+				oldmember.setPass(pass);
+			}
+			
+			final Integer roleId = newmember.getRoleId(); 
+			if (roleId != null) {
+				oldmember.setRoleId(roleId);
+			}
+			
+			
+			transaction.commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return -1;
+		}
+	}
+	
+	public Member selectById(Integer id) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member member = session.get(Member.class, id);
+			
+			transaction.commit();
+			return member;
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return null;
 		}
 	}
 }
